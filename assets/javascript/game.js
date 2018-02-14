41c97bb178df5d603cbcd938b0d906775fa6b0e7
Joseph Bergen <joseph.bergen@icloud.com>
2018-02-13 20:37:53 -0700

    updated code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 $( document ).ready(function(){
     var Random=Math.floor(Math.random()*101+19)
     // Selects a random number to be shown at the start of the game
-    // Number should be should be between 19 - 120
+    // Number should be between 19 - 120
     //
     $('#randomNumber').text(Random);
     // Appending random number to the randomNumber id in the html doc
